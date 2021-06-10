f1517d5d5485405570551dfd290ba927e3223229
Amulya Mohan <amulyamo@hotmail.com>
2021-06-10 00:18:42 -0700

    ye now we got the blite anim in the menu
    but the x values in the xml are off someone pls fix

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -53,7 +53,7 @@ class MenuCharacter extends FlxSprite
 		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
 		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
 		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
-		animation.addByPrefix('blite', "Mom Idle BLACK LINES", 24);
+		animation.addByPrefix('blite', "blite dance idle BLACK LINES", 24);
 
 		setGraphicSize(Std.int(width * scale));
 		updateHitbox();
