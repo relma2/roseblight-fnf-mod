bd1896e724d4fe7973d7737200fce8b55c437347
Amulya Mohan <amulyamo@hotmail.com>
2021-06-15 22:34:47 -0700

    WE DID IT FRIENDS

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -183,11 +183,12 @@ class TitleState extends MusicBeatState
 			// logoBl.color = FlxColor.BLACK;
 		}
 
-		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+		gfDance = new FlxSprite(FlxG.width * 0.5, FlxG.height * 0.02);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
+		gfDance.scale.set(0.75,0.75);
 		add(gfDance);
 		add(logoBl);
 
