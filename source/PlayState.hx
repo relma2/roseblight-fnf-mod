c4b69e66604b07855f5f27487a14248894465f70
Amulya Mohan <amulyamo@hotmail.com>
2021-06-19 18:42:37 -0700

    initial stage setup test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -710,7 +710,7 @@ class PlayState extends MusicBeatState
 			// placeholder for normal stage
 			case 'gray' | 'grayEvil':
 				{
-					curStage = 'gray'
+					curStage = 'gray';
 					var bg:FlxSprite = new FlxSprite(-400, -500).loadGraphic(Paths.image('background_mountain.png','week7'));
 					bg.antialiasing = true;
 					bg.scrollFactor.set(0.2, 0.2);
