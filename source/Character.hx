8c3d1477e0b7951d2e3b5e4b687ed1f5748c55f7
Amulya Mohan <amulyamo@hotmail.com>
2021-06-29 22:59:28 -0700

    sfx and better asset

--- source/Character.hx
+++ source/Character.hx
@@ -42,8 +42,8 @@ class Character extends FlxSprite
 				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
 				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-				animation.addByIndices('hairBlow', "QF Dancing Beat Blowing", [0, 1, 2, 3], "", 24);
-				animation.addByIndices('hairFall', "GF Dancing Beat Blowing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
+				animation.addByIndices('hairBlow', "GF Dancing Beat Blowing", [0, 1, 2, 3], "", 24);
+				animation.addByIndices('hairFall', "QF Dancing Beat Blowing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
 				animation.addByPrefix('scared', 'GF FEAR', 24);
 
 				addOffset('cheer');
@@ -58,7 +58,7 @@ class Character extends FlxSprite
 				addOffset('hairBlow', 45, -8);
 				addOffset('hairFall', 0, -9);
 
-				addOffset('scared', -2, -17);
+				addOffset('scared', -2, -100);
 
 				playAnim('danceRight');
 
