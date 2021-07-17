d87b500c83405def58d6c375474f62e3d9f4965e
Amulya Mohan <amulyamo@hotmail.com>
2021-07-17 00:49:11 -0700

    freeze chart too

--- assets/preload/data/aplovecraft/modchart.lua
+++
@@ -1,9 +0,0 @@
-function pausa(missed)
-    if (missed) then
-        playPausa(true)
-    else
-        playPausa(false)
-    end
-    playActorAnimation('girlfriend', 'scared', true, false)
-    playActorAnimation('dad', 'pausa', true, false)
-end
