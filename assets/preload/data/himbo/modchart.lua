89af3e42e17203dca27b11c7f57b0112871f0072
Amulya Mohan <amulyamo@hotmail.com>
2021-07-22 01:11:56 -0700

    refine lua; correct beatnums

--- assets/preload/data/himbo/modchart.lua
+++ assets/preload/data/himbo/modchart.lua
@@ -1,15 +1,18 @@
-shopReverb = 100
-shopBreak = 100 + 10
+shopReverb = 292
+shopBreak = 300
 
 function beatHit(beat)
     if (beat == shopReverb) then 
-        print("begin moving camera to shop window")
+        print("begin moving camera to focus on shop window")
     elseif (beat == shopReverb+1) then
         print("shake the shop sprite like realy fast")
+    elseif (beat > shopReverb + 1 and beat < shopBreak - 1) then
+        print("continue moving camera hud to shop window focus")
+    elseif (beat == shopBreak-1) then
+	    print("finish moving camera to shop window focus")
     elseif (beat == shopBreak) then
-	print("change shop sprite to window broken; play crash sfx")
-    elseif (beat == shopBreak + 1) then
-	print("begin moving camera Hud to normal position")
+	    print("shatter shop window by changing sprite to shop_broken\nshop stops shaking")
+        print("play the shatter glass sfx")
     else
 	-- do fucking nothing
     end
