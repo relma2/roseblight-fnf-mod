5f215c9088157fadf20c395c532de36c22ad11bf
Amulya Mohan <amulyamo@hotmail.com>
2021-07-25 01:20:18 -0700

    barble time

--- source/BackgroundDancer.hx
+++ source/BackgroundDancer.hx
@@ -8,13 +8,17 @@ class BackgroundDancer extends FlxSprite
 	public function new(x:Float, y:Float, week:Int = 4)
 	{
 		super(x, y);
+		var path:String = "limo/limoDancer";
+		var name:String = "bg dancer sketch PINK";
 
-		if (week==7)
-			frames = Paths.getSparrowAtlas("griswell/barbleDancer",'week7');
-		else
-			frames = Paths.getSparrowAtlas("limo/limoDancer",'week4');
-		animation.addByIndices('danceLeft', 'bg dancer sketch PINK', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		animation.addByIndices('danceRight', 'bg dancer sketch PINK', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		if (week == 7)
+		{
+			path = "griswell/barbleDancer";
+			name = "barble_instance";
+		}
+		frames = Paths.getSparrowAtlas(path, "week" + week);
+		animation.addByIndices('danceLeft', name, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+		animation.addByIndices('danceRight', name, [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		animation.play('danceLeft');
 		antialiasing = true;
 	}
