27d174a4218439c1f97a39585ba7c4d445e32e73
Amulya Mohan <amulyamo@hotmail.com>
2021-07-28 01:52:29 -0700

    yaya shop breaks on normal

--- assets/preload/data/himbo/modchart.lua
+++ assets/preload/data/himbo/modchart.lua
@@ -1,19 +1,22 @@
 shopReverb = 292
-shopBreak = 300
+shopBreak = 299
 
 function beatHit(beat)
     if (beat == shopReverb) then 
         print("begin moving camera to focus on shop window")
     elseif (beat == shopReverb+1) then
         print("shake the shop sprite like realy fast")
+        print("shake the \"shopbg\" sprite fast, but slightly less intense than the shop")
     elseif (beat > shopReverb + 1 and beat < shopBreak - 1) then
+        print("continue shaking")
         print("continue moving camera hud to shop window focus")
     elseif (beat == shopBreak-1) then
+        print("continue shaking")
 	    print("finish moving camera to shop window focus")
     elseif (beat == shopBreak) then
-	    print("shatter shop window by changing sprite to shop_broken\nshop stops shaking")
-        print("play the shatter glass sfx")
+	    -- i changed the shop prite and played the sfx in the main code
+        print ("stop shaking sprites")
     else
-	-- do fucking nothing
+	    -- do fucking nothing
     end
 end
