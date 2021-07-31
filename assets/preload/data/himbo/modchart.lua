b1f743d74624665fc313aa0e359085e595d07101
Amulya Mohan <amulyamo@hotmail.com>
2021-07-31 03:15:43 -0700

    what i have

--- assets/preload/data/himbo/modchart.lua
+++ assets/preload/data/himbo/modchart.lua
@@ -1,22 +1,29 @@
 shopReverb = 292
 shopBreak = 299
 
+myBeat = 0
+
+shop_x = -1
+shop_y = -1
+trees_x = -1
+trees_y = -1
+
+function start(song)
+    shop_x = getActorX("shop")
+    shop_y = getActorY("shop")
+    print("record shop  " .. shop_x .. ", " .. shop_y)
+    trees_x = getActorX("shopbg")
+    trees_y = getActorY("shopbg")
+    print("record sprites  " .. trees_x .. ", " .. trees_y)
+end
+
 function beatHit(beat)
+    myBeat = beat
     if (beat == shopReverb) then 
         print("begin moving camera to focus on shop window")
-    elseif (beat == shopReverb+1) then
-        print("shake the shop sprite like realy fast")
-        print("shake the \"shopbg\" sprite fast, but slightly less intense than the shop")
-    elseif (beat > shopReverb + 1 and beat < shopBreak - 1) then
-        print("continue shaking")
-        print("continue moving camera hud to shop window focus")
-    elseif (beat == shopBreak-1) then
-        print("continue shaking")
-	    print("finish moving camera to shop window focus")
-    elseif (beat == shopBreak) then
-	    -- i changed the shop prite and played the sfx in the main code
-        print ("stop shaking sprites")
-    else
-	    -- do fucking nothing
+        tweenCameraPos(600, -200, 2.4, "nothing")  
     end
+end
+
+function nothing(camera)
 end
